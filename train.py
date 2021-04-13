e6a4ff48b15f54a53b403e37034c8465b2028638
Klever <kgopar20@wooster.edu>
2021-04-12 21:08:56 -0400

    set up, train, and save the neural network model

--- train.py
+++ train.py
@@ -68,6 +68,45 @@ padded_sequences = pad_sequences(sequences, truncating='post', maxlen=20)
 
 # set up and define the neural network model
 model = Sequential()
+# add the embedding layer used to set up and update weights for each of the
+# integer vectors(words). Weights can be retrieved by multiplying the one-hot
+# vector assigned to each integer vector to the embedding matrix.
+model.add(Embedding(1000, 16, input_length=20))
+# ( Need to revisit this )
+model.add(GlobalAveragePooling1D())
+# a neural network layer is added to the model that creates a weights matrix.
+# This layer implements the output = activation(dot(input, kernel) + bias
+# operation where the kernel is the weights matrix and the activation is the
+# rectified linear activation function.
+model.add(Dense(16, activation='relu'))
+# another neural network layer is added same as before
+model.add(Dense(16, activation='relu'))
+# another neural network is added, but the activation function is softmax
+# which converts the previous output to a vector of categorical probabilities
+model.add(Dense(num_labels, activation='softmax'))
+# ( need to revisit this) sets up the configurations for the neural network
+# model. The Adam gradient descent method is used.
+# sparse_categorical_crossentropy is used to get the crossentropy loss between
+# predictions and labels. accuracy in metrics is used to get how often labels
+# are predicted corrrectly
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+# a summary of the neural network gets printed
+model.summary()
 
+# the neural network model gets trained for 500 iterations
+# and a record of loss and metric values is given with a History object
+# returned
+history = model.fit(padded_sequences, np.array(training_labels), epochs=500)
 
+# the neural network model gets saved to TensorFlow SavedModel
+model.save("chat_model")
+
+# the tokenizer used to vectorize the data gets saved using the pickle module
+with open('tokenizer.pickle', 'wb') as tokenizer_file:
+    pickle.dump(tokenizer, tokenizer_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+# the label encoder with the encoded target labels gets saved as well
+with open('label_encoder', 'wb') as encoder_file:
+    pickle.dump(label_encoder, encoder_file, protocol=pickle.HIGHEST_PROTOCOL)
 
